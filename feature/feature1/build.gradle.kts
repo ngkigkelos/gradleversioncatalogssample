fc54fa78f3cb1ebd8678915998f7f3daa6827ec1
Nikos Gkigkelos <n.gkigkelos@withplum.com>
2024-03-01 13:30:39 +0200

    apply custom plugin

--- feature/feature1/build.gradle.kts
+++ feature/feature1/build.gradle.kts
@@ -1,32 +1,9 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.gradleversioncatalogssample.android.library)
 }
 
 android {
     namespace = "com.nikosgig.feature1"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
-    }
-    kotlinOptions {
-        jvmTarget = "19"
-    }
 }
 
 dependencies {
